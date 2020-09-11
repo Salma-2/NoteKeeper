089eb3332348105c23925cd8e1d79449b7f7c1de
Salma-2 <salmahamdisaad@gmail.com>
2020-09-11 04:16:33 +0200

    add NoteReminderReceiver

---
+++ app/src/main/java/com/example/notekeeper/NoteReminderReceiver.java
@@ -0,0 +1,20 @@
+package com.example.notekeeper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NoteReminderReceiver extends BroadcastReceiver {
+    public static final String EXTRA_NOTE_ID = "com.example.notekeeper.extra.NOTE_ID";
+    public static final String EXTRA_NOTE_TITLE = "com.example.notekeeper.extra.NOTE_TITLE";
+    public static final String EXTRA_NOTE_TEXT = "com.example.notekeeper.extra.NOTE_TEXT";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int noteId = intent.getIntExtra( EXTRA_NOTE_ID, 0 );
+        String noteTitle = intent.getStringExtra( EXTRA_NOTE_TITLE );
+        String noteText = intent.getStringExtra( EXTRA_NOTE_TEXT );
+
+        NoteReminderNotification.notify( context, noteTitle, noteText, noteId );
+    }
+}
